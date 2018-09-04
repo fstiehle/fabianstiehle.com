2b6b3391671afb927d81a90c290dd3dbec51ed07
Fabian Stiehle <f.stiehle@gmail.com>
2018-09-04 17:27:11 +0200

    improve footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => (
   <footer className="card card--wmargin">
     <div className="card__content card__content--constrained">
-      <h3>Do you like what you see? <a href="https://www.buymeacoffee.com/Fabi">Buy me a coffe.</a></h3>
+      <div style={{marginBottom: 10}}>Do you like what you see? <a href="https://www.buymeacoffee.com/Fabi">Buy me a coffe.</a></div>
       <div className="footer-bar">
         <span className="footer-bar__item">
           <a href="#top"><i className="icon-up-open-big"></i> Back to top</a>
